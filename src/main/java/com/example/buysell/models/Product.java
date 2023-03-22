b6d7f7eec81cc58ebe0879a3a550e74e319159ce
Rasul <76030734+Rasul-max-oss@users.noreply.github.com>
2023-03-22 12:29:11 +0800

    first commit

---
+++ src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,59 @@
+package com.example.buysell.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "mileage")
+    private Long mileage;//Пробег
+    @Column(name = "color")
+    private  String color;//цвет
+    @Column(name = "Steeringwheel")
+    private String Steeringwheel;//Руль
+    @Column(name = "engine")
+    private String engine;//двигатель
+    @Column(name = "Transmission")
+    private  String Transmission;//Коробка передач
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+    mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
+}
---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,68 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Image;
+import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+    //Если есть title то ищем
+    // Если  в заголовке ничего нет,  возвращаем  список .
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
+    }
+
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);// преобразуем в изображение
+            image1.setPreviewImage(true);// так как это первое изображение, она и станет превьюшной.
+            product.addImageToProduct(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+        //Cохраняем товар и логируем.
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+        Product productFromDb = productRepository.save(product);//Обновление
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
+        productRepository.save(product);
+    }
+    //Метод преобразования
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }//Удаление
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
